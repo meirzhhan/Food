a37f360047959b1660a5472933fe5b472f26c0b4
meirzhhan <mbeymishev@mail.ru>
2023-11-03 01:13:39 +0600

    053 Реализация скрипта отправки данных на сервер

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 window.addEventListener('DOMContentLoaded', () => {
 
     // Tabs
@@ -215,6 +216,61 @@ window.addEventListener('DOMContentLoaded', () => {
         '.menu .container',
         'menu__item'
     ).render();
+
+    // Forms
+    // Shift + F5
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо! Скоро мы с вам свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage); 
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            // request.setRequestHeader('Content-type', 'multipart/form-data'); Когда мы используем XMLHttpResponse + Объект + form-data, нам заголовок устанавливается автоматически. не нужно вручную устанавливать XML format
+            request.setRequestHeader('Content-type', 'application/json'); //JSON format
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(value, key){
+                object[key] = value;
+            }); //Объект для JSON. JSON не принимает formData
+
+            const json = JSON.stringify(object);
+
+            // request.send(formData);XML PHP
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
     
 });
 
